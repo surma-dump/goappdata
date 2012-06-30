09c6ba7a2b19aa1c1a271006f0f01957088953dc
Alexander Surma <surma@surmair.de>
2012-06-30 18:59:23 +0200

    Add CreatePath method

--- common.go
+++ common.go
@@ -13,10 +13,17 @@ package goappdata
 
 import (
 	"fmt"
+	"os"
 )
 
 // Returns the path to folder in which the an app with the name `appname` is
 // supposed to store its data.
 func Path(appname string) string {
 	return fmt.Sprintf(format, root, appname)
+}
+
+// Same as Path, but implicitly creates the folder, if it does not exist yet.
+func CreatePath(appname string) (string, error) {
+	var dir = Path(appname)
+	return dir, os.MkdirAll(Path(appname), os.FileMode(0750))
 }
